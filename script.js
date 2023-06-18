b1bedb191560ccc3e7138b9587bba84c3416f188
Robert Lazić <robert-lazicpz@gmail.com>
2023-06-18 18:24:32 +0200

    modified:   assets/styles/sass/base/_base.scss 	modified:   assets/styles/sass/components/_button.scss 	modified:   assets/styles/sass/layout/_header.scss 	modified:   assets/styles/sass/layout/_main-section.scss 	modified:   assets/styles/sass/utilities/_variables.scss 	modified:   assets/styles/style.css 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const menuBtn = document.querySelector('.menu');
+const menuBtn = document.querySelector('.nav-toggle');
+const navigation = document.querySelector('.nav');
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('opened');
+  navigation.classList.toggle('nav--visible');
 });
